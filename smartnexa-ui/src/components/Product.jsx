41198c8b99ded07aab71a0cc8bb82c1ec49d6e6a
MuhdAdil13 <adilrahim333@gmail.com>
2024-10-10 11:58:05 +0530

    Add Product Component

--- smartnexa-ui/src/components/Product.jsx
+++ smartnexa-ui/src/components/Product.jsx
@@ -2,33 +2,59 @@ import React from 'react'
 
 const Product = () => {
     const containerStyle={
-        backgroundColor :'rgb(246,246,246)',
-        width:'25rem',
-        height:'30rem'
+        // backgroundColor :'rgb(240,240,240)',
+        margin:'1rem',
+        width:'13rem',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-evenly',
+        borderRadius:'5px',
+        border:'1px solid rgba(0,0,0,0.2)',
     };
+
+    const productDetails={
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between',
+        marginTop:'7px'
+    }
+
+    const imgStyle = {
+        margin:'3px',
+        borderRadius:'4px'
+    }
 
     const shortDescription={
         color:'black',
-        fontSize:'1.2rem'
+        fontSize:'1rem',
+        margin:'0px 15px',
+        textAlign:'justify'
     }
 
     const userRatings = {
         color:'black',
-        fontSize:'1rem'
+        fontSize:'1rem',
+        marginTop:'3px',
+        textAlign:'center'
     }
 
     const price={
         color:'black',
-        fontSize:'1rem'
+        fontSize:'1rem',
+        margin:'3px 0px',
+        textAlign:'center'
     }
 
   return (
     <>
         <div style={containerStyle}>
-            <img src="" alt="" />
-            <div style={shortDescription}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam saepe facere aliquam unde excepturi quasi! Repudiandae, molestias nulla, sapiente explicabo possimus nesciunt iusto consectetur dicta earum veritatis quibusdam odit voluptate!</div>
-            <div style={userRatings}>4.5</div>
-            <div style={price}>5678.99</div>
+            <img src="https://picsum.photos/100" style={imgStyle} alt="A random Image😊" />
+            <div style={productDetails}>
+                <p style={shortDescription}>Lorem ipsum dolor sit, amet consectetur adipisicing 
+                elit. </p>
+                <p style={userRatings}>4.5 ⭐</p>
+                <p style={price}>5678.99</p>
+            </div>
         </div>
     </>
   )
