37441edbf01c35707c122f4c80af1ee5c76ebf55
MuhdAdil13 <adilrahim333@gmail.com>
2024-10-15 12:57:25 +0530

    Add Navbar style

--- smartnexa-ui/src/components/Navbar.jsx
+++ smartnexa-ui/src/components/Navbar.jsx
@@ -1,39 +1,40 @@
 import React from 'react'
+import './Navbar.css'
+import SearchIcon from './icons/search.svg'
+import PersonIcon from './icons/person.svg'
+import CartIcon from './icons/cart.svg'
+import DropDown from './icons/dropdown.svg'
+
 
 const Navbar = () => {
-    const navbarContainer = {
-        display : 'flex',
-        justifyContent : 'space-between',
-        alignItems:'center',    
-        height:'4rem',
-        padding:'0 2rem',      
-        backgroundColor:'dodgerblue',
-        color:'white'
-    }
-
-    const brandName = {
-
-    }
-
-    const links = {
-        display : 'flex',
-        listStyleType : 'none',
-        gap:'2rem'
-    }      
-
   return (          
     <>
-        <div style={navbarContainer} >
-            <div style={brandName}>SmartNexa</div>
+        <div className='navbar-container' >
+            <div className='brand-name'>SmartNexa</div>
             <div>
-                <ul style={links}>
-                    <li>Products</li>
+                <ul className='links'>
+                    <li className='dropdown-container'>
+                      <div>Products</div>
+                      <div><img src={DropDown} alt="dropdown" width={21} height={21}/></div>
+                      </li> 
                     <li>Deals</li>
                     <li>Guides</li>
-                    <li>Support</li>            
-                    <li>Cart</li>
-                    <li>Login/Signup</li>
+                    <li className='dropdown-container'>
+                      <div>Support</div>
+                      <div><img src={DropDown} alt="dropdown" width={21} height={21}/></div>
+                    </li>            
                 </ul>
+            </div>
+            <div className='icon-container'>
+                <a href="">
+                  <img src={SearchIcon} alt="Search Icon" width={21} height={21} />
+                  </a>
+                <a href="">
+                  <img src={PersonIcon} alt="Person Icon" width={21} height={21} />
+                </a>
+                <a href="" className='cart-container'>
+                    <img src={CartIcon} alt="Cart Icon" width={21} height={21}/>
+                </a>
             </div>
         </div>
     </>
